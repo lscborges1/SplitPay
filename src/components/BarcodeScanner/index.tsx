a9b7712d46a6b551f2709291f22c2b916bb92136
Lucas Borges <lscborges1@gmail.com>
2020-10-29 23:45:08 -0300

    Initial commit

---
+++ src/components/BarcodeScanner/index.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+interface IQRCodeData {
+  type: number,
+  data: string
+}
+
+export default function BarcodeScanner() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [] = useState('')
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }:IQRCodeData) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+      }}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    </View>
+  );
+}
---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import BarcodeScanner from '../../components/BarcodeScanner'
+
+function Landing(){
+  return <BarcodeScanner/>
+}
+export default Landing;
